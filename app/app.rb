9ec4224d6d9b4d591d4596177924d6f8c31864fe
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-03 18:04:06 +0100

    Added the user class, added layout view, requires bcrypt to store passwords safely

--- app/app.rb
+++ app/app.rb
@@ -4,6 +4,8 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class BookmarkManagerWeb < Sinatra::Base
+  enable :sessions
+  set :sessions_secret, 'super secret'
 
   get '/links' do
     @links = Link.all
@@ -27,6 +29,23 @@ class BookmarkManagerWeb < Sinatra::Base
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
     erb :'links/index'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                password: params[:password])
+    session[:user_id] = user.id
+    redirect '/links'
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
   end
 
   run! if app_file == $0
